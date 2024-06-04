9842924cb793240f4ea085626bdced1421026a01
GregoryMoutote <gregory.moutote@gmail.com>
2024-06-04 15:23:21 +0200

    WIP SA analysis + greedy

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -165,6 +165,8 @@ def simulated_annealing(vrptw: VRPTW, t0_param: int = None, mu_param: float = No
         n1 = log(delta_f)
     elif way == "2SQRT":
         n1 = 2 * sqrt(delta_f)
+    elif way == "10SQRT":
+        n1 = 10 * sqrt(delta_f)
     elif way == "2LN":
         n1 = 2 * log(delta_f)
     else:
@@ -235,123 +237,151 @@ def simulated_annealing(vrptw: VRPTW, t0_param: int = None, mu_param: float = No
             continue
     return (x_min, mu, t_0, n1) if kwargs.get("return_mu") else x_min
 
-VRPTW_ = VRPTW('data101.vrp')
 # VRPTW_ = VRPTW('data101 short.vrp')
 
 SIM = True
 
 mus_full = [0.5,0.6,0.7,0.75,0.8,0.85,0.9,0.91,0.92,0.93,0.94,0.95,0.96,0.97,0.98,0.99]
 mus = [mus_full[5]]
+
+files = [
+    "101",
+    "101 short",
+    # "102",
+    # "102 short",
+    # "111",
+    # "111 short",
+    # "112",
+    # "112 short",
+    # "201",
+    # "201 short",
+    # "202",
+    # "202 short",
+    # "1101",
+    # "1101 short",
+    # "1102",
+    # "1102 short",
+    # "1201",
+    # "1201 short",
+    # "1202",
+    # "1202 short",
+]
+
 if SIM:
-    last_line = None
-    try:
-        with open(f"./100_first_101_tw/sim.csv", "r", newline="") as file:
-            lines = file.readlines()
-            if len(lines) >= 2:
-                last_line = lines[-1]
-    except Exception:
-        pass
-    if not last_line:
-        with open(f"./100_first_101_tw/sim.csv", "w", newline="") as file:
-            csv.writer(file).writerow(["AVG_MU", "MIN_MU", "MAX_MU", "OPT_USED", "N2", "AVG_T0", "MIN_T0", "MAX_T0", "F0", "F_AVG", "F_MIN", "F_MAX", "AVG_BEST_AT", "MIN_BEST_AT", "MAX_BEST_AT", "INIT_NB_TRUCKS", "AVG_NB_TRUCKS", "MIN_NB_TRUCKS", "MAX_NB_TRUCKS", "AVG_EXEC_T", "MIN_EXEC_T", "MAX_EXEC_T","AVG_N1", "MIN_N1", "MAX_N1", "WAY"])
+    for data_file in files:
+        VRPTW_ = VRPTW(f'data{data_file}.vrp')
+        last_line = None
+        try:
+            with open(f"./100_first_101/sim_{data_file}.csv", "r", newline="") as file:
+                lines = file.readlines()
+                if len(lines) >= 2:
+                    last_line = lines[-1]
+        except Exception:
+            pass
+        if not last_line:
+            with open(f"./100_first_101/sim_{data_file}.csv", "w", newline="") as file:
+                csv.writer(file).writerow(["AVG_MU", "MIN_MU", "MAX_MU", "OPT_USED", "N2", "AVG_T0", "MIN_T0", "MAX_T0", "F0", "F_AVG", "F_MIN", "F_MAX", "AVG_BEST_AT", "MIN_BEST_AT", "MAX_BEST_AT", "INIT_NB_TRUCKS", "AVG_NB_TRUCKS", "MIN_NB_TRUCKS", "MAX_NB_TRUCKS", "AVG_EXEC_T", "MIN_EXEC_T", "MAX_EXEC_T","AVG_N1", "MIN_N1", "MAX_N1", "WAY"])
 
-    params = list()
-    opt_used = ["RSE"]
-    # opt_used = ["RS", "RSE"]
-    # opt_used = ["R", "RS", "RE", "RSE", "RI", "RISE"]
-    n2s = [10000]
-    # n2s = [10000, 1000]
-    way_to_get_n1 = ["SQRT"]
-    # way_to_get_n1 = ["SQRT", "LN", "2SQRT", "2LN"]
-    t_n1s = [0.86]
-    # t0s = [3,4,5,6,7,8,9,10,15,20,25,30,40,50,60,70,80,90,100,150,200,300,400,500,1000]
+        params = list()
+        # opt_used = ["RSE"]
+        opt_used = ["RS"]
+        # opt_used = ["RS", "RSE"]
+        # opt_used = ["R", "RS", "RE", "RSE", "RI", "RISE"]
+        n2s = [10000]
+        # n2s = [10000, 1000]
+        way_to_get_n1 = ["SQRT"]
+        # way_to_get_n1 = ["SQRT", "LN", "2SQRT", "2LN"]
+        t_n1s = [0.86]
+        # t_n1s = [0.86]
+        # t0s = [3,4,5,6,7,8,9,10,15,20,25,30,40,50,60,70,80,90,100,150,200,300,400,500,1000]
 
-    for opt in opt_used:
-        for n2 in n2s:
-            for t_n1 in t_n1s:
-                for way in way_to_get_n1:
-                    params.append((opt, n2, t_n1, way))
+        for opt in opt_used:
+            for n2 in n2s:
+                for t_n1 in t_n1s:
+                    for way in way_to_get_n1:
+                        params.append((opt, n2, t_n1, way))
 
-    last_dones = (0,0,0,0)
-    if last_line:
-        split_line = last_line.split(",")
-        last_way = str(split_line[-1])
-        last_opt = str(split_line[3])
-        last_n2 = int(split_line[4])
-        # last_t0 = int(split_line[3])
-        last_dones = (last_opt, last_n2, t_n1s[0], last_way.split("/")[0])
-    
-    try:
-        params = params[params.index(last_dones) + 1:]
-    except ValueError:
-        pass
+        last_dones = (0,0,0,0)
+        if last_line:
+            split_line = last_line.split(",")
+            last_way = str(split_line[-1])
+            last_opt = str(split_line[3])
+            last_n2 = int(split_line[4])
+            # last_t0 = int(split_line[3])
+            last_dones = (last_opt, last_n2, t_n1s[0], last_way.split("/")[0])
+        
+        try:
+            params = params[params.index(last_dones) + 1:]
+        except ValueError:
+            pass
 
-    for param in params:
-        opt = param[0]
-        n2 = param[1]
-        t_n1 = param[2]
-        way = param[3]
+        for param in params:
+            opt = param[0]
+            n2 = param[1]
+            t_n1 = param[2]
+            way = param[3]
 
-        print(f"Doing {opt} {n2} {t_n1} {way}")
+            print(f"Doing {opt} {n2} {t_n1} {way}")
 
-        # ALLOWED_OPERATORS = OPT_MAP.get(opt, {relocate_delivery: [2]})
+            # ALLOWED_OPERATORS = OPT_MAP.get(opt, {relocate_delivery: [2]})
 
-        fs = list()
-        trucks = list()
-        best_ats = list()
-        exec_times = list()
-        computed_mus = list()
-        t_0s = list()
-        n_1s = list()
-        for _ in range(5):
-            try:
-                begin = time.time_ns()
-                vrptw, computed_mu, computed_t0, computed_n1 = simulated_annealing(VRPTW_, n2_param=n2, t_final=t_n1, **{"opt": opt, "way": way, "return_mu": True})
-                computed_mus.append(computed_mu)
-                n_1s.append(computed_n1)
-                t_0s.append(computed_t0)
-                end = time.time_ns()
-                fs.append(fitness(vrptw))
-                best_ats.append(BEST_AT[0])
-                trucks.append(len(vrptw.routes))
-                exec_times.append(end - begin)
-            except Exception as e:
-                print(e)
+            fs = list()
+            trucks = list()
+            best_ats = list()
+            exec_times = list()
+            computed_mus = list()
+            t_0s = list()
+            n_1s = list()
+            for _ in range(5):
+                try:
+                    begin = time.time_ns()
+                    vrptw, computed_mu, computed_t0, computed_n1 = simulated_annealing(VRPTW_, n2_param=n2, t_final=t_n1, **{"opt": opt, "way": way, "return_mu": True})
+                    computed_mus.append(computed_mu)
+                    n_1s.append(computed_n1)
+                    t_0s.append(computed_t0)
+                    end = time.time_ns()
+                    fs.append(fitness(vrptw))
+                    best_ats.append(BEST_AT[0])
+                    trucks.append(len(vrptw.routes))
+                    exec_times.append(end - begin)
+                except Exception as e:
+                    print(e)
+                    continue
+
+            if not fs or not best_ats or not trucks:
+                print("Params gave no results")
                 continue
 
-        if not fs or not best_ats or not trucks:
-            print("Params gave no results")
-            continue
+            f0 = fitness(VRPTW_)
+            f_avg = sum(fs) / len(fs)
+            f_min = min(fs)
+            f_max = max(fs)
+            n1_avg = sum(n_1s) / len(n_1s)
+            n1_min = min(n_1s)
+            n1_max = max(n_1s)
+            t0_avg = sum(t_0s) / len(t_0s)
+            t0_min = min(t_0s)
+            t0_max = max(t_0s)
+            mu_avg = sum(computed_mus) / len(computed_mus)
+            mu_min = min(computed_mus)
+            mu_max = max(computed_mus)
+            avg_best_at = sum(best_ats) / len(best_ats)
+            min_best_at = min(best_ats)
+            max_best_at = max(best_ats)
+            init_nb_trucks = len(VRPTW_.routes)
+            avg_nb_trucks = sum(trucks) / len(trucks)
+            min_nb_trucks = min(trucks)
+            max_nb_trucks = max(trucks)
+            avg_exec_t = sum(exec_times) / len(exec_times)
+            min_exec_t = min(exec_times)
+            max_exec_t = max(exec_times)
 
-        f0 = fitness(VRPTW_)
-        f_avg = sum(fs) / len(fs)
-        f_min = min(fs)
-        f_max = max(fs)
-        n1_avg = sum(n_1s) / len(n_1s)
-        n1_min = min(n_1s)
-        n1_max = max(n_1s)
-        t0_avg = sum(t_0s) / len(t_0s)
-        t0_min = min(t_0s)
-        t0_max = max(t_0s)
-        mu_avg = sum(computed_mus) / len(computed_mus)
-        mu_min = min(computed_mus)
-        mu_max = max(computed_mus)
-        avg_best_at = sum(best_ats) / len(best_ats)
-        min_best_at = min(best_ats)
-        max_best_at = max(best_ats)
-        init_nb_trucks = len(VRPTW_.routes)
-        avg_nb_trucks = sum(trucks) / len(trucks)
-        min_nb_trucks = min(trucks)
-        max_nb_trucks = max(trucks)
-        avg_exec_t = sum(exec_times) / len(exec_times)
-        min_exec_t = min(exec_times)
-        max_exec_t = max(exec_times)
-
-        with open(f"./100_first_101_tw/sim.csv", "a", newline="") as file:
-            csv.writer(file).writerow([mu_avg, mu_min, mu_max, opt, n2, t0_avg, t0_min, t0_max, f0, f_avg, f_min, f_max, avg_best_at, min_best_at, max_best_at, init_nb_trucks, avg_nb_trucks, min_nb_trucks, max_nb_trucks, avg_exec_t, min_exec_t, max_exec_t, n1_avg, n1_min, n1_max, way])
+            with open(f"./100_first_101/sim_{data_file}.csv", "a", newline="") as file:
+                csv.writer(file).writerow([mu_avg, mu_min, mu_max, opt, n2, t0_avg, t0_min, t0_max, f0, f_avg, f_min, f_max, avg_best_at, min_best_at, max_best_at, init_nb_trucks, avg_nb_trucks, min_nb_trucks, max_nb_trucks, avg_exec_t, min_exec_t, max_exec_t, n1_avg, n1_min, n1_max, way])
 
 
 else:
+    VRPTW_ = VRPTW("data101.vrp")
     begin = time.time()
     vrptw = simulated_annealing(VRPTW_, t_final=0.86)
     print(f"Time took : {time.time() - begin}")
